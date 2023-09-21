90c8198a260fc7aedf7b4e929f58f380f5e0f6d5
STUDENT Steve Lloyd <Steve.Lloyd@students.ittralee.ie>
2023-09-21 10:59:33 +0100

    Signed-off-by: STUDENT Steve Lloyd <Steve.Lloyd@students.ittralee.ie>

---
+++ My project/Assets/CubeControlScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeControlScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 7, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown) (KeyCode.W) 
+        {
+            transform.position += Vector3.forward * Time.deltaTime;
+        }
+       
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up,90) * Time.deltaTime;
+        }
+    }
+}
